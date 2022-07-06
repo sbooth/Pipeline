963396e8c68ce1c34fe93b4126c42e24ab09cec8
Stephen F. Booth <me@sbooth.org>
2022-07-06 10:23:24 -0500

    Change ParameterValue to SQLParameter

--- Tests/PipelinePerformanceTests/PipelinePerformanceTests.swift
+++ Tests/PipelinePerformanceTests/PipelinePerformanceTests.swift
@@ -114,7 +114,7 @@ class PipelinePerformanceTests: XCTestCase {
 
 			var s = try! db.prepare(sql: "insert into t1(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
 
-			let values: [ParameterValue] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+			let values: [SQLParameter] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
 			startMeasuring()
 
