82aac77d62017b574a4539a0b961f0b345c9ad5a
Stephen F. Booth <me@sbooth.org>
2022-07-08 16:37:29 -0500

    Update tests

--- Tests/PipelinePerformanceTests/PipelinePerformanceTests.swift
+++ Tests/PipelinePerformanceTests/PipelinePerformanceTests.swift
@@ -56,8 +56,8 @@ class PipelinePerformanceTests: XCTestCase {
 
 			let rowCount = 50_000
 			for i in 0..<rowCount {
-				try! s.bind(value: .integer(Int64(i*2)), toParameter: 1)
-				try! s.bind(value: .integer(Int64(i*2+1)), toParameter: 2)
+				try! s.bind(.int(i*2), toParameter: 1)
+				try! s.bind(.int(i*2+1), toParameter: 2)
 
 				try! s.execute()
 
@@ -113,37 +113,6 @@ class PipelinePerformanceTests: XCTestCase {
 			var s = try! db.prepare(sql: "insert into t1(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
 
 			let values: [DatabaseValue] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-
-			startMeasuring()
-
-			let rowCount = 10_000
-			for _ in 0..<rowCount {
-				try! s.bind(values: values)
-
-				try! s.execute()
-
-				try! s.clearBindings()
-				try! s.reset()
-			}
-
-			stopMeasuring()
-
-			s = try! db.prepare(sql: "select count(*) from t1;")
-			let count = try! s.step()!.value(at: 0, .int)
-
-			XCTAssertEqual(count, rowCount)
-		}
-	}
-
-	func testPipelineInsertPerformance_3_2() {
-		self.measureMetrics(XCTestCase.defaultPerformanceMetrics, automaticallyStartMeasuring: false) {
-			let db = try! Database()
-
-			try! db.execute(sql: "create table t1(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);")
-
-			var s = try! db.prepare(sql: "insert into t1(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
-
-			let values: [SQLParameter] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
 			startMeasuring()
 
