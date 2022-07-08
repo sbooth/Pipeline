ea7b27e3d4a1e7404e44285bfe79ea111cdcf8ac
Stephen F. Booth <me@sbooth.org>
2022-07-08 10:08:01 -0500

    Remove parameter names for basic binding funcs

--- Sources/Pipeline/SQLParameterBinder.swift
+++ Sources/Pipeline/SQLParameterBinder.swift
@@ -42,8 +42,11 @@ extension Statement {
 	/// - parameter binder: The `SQLParameterBinder` to use for binding `value`.
 	///
 	/// - throws: An error if `value` couldn't be bound.
-	public func bind<T>(_ value: T, toParameter index: Int, _ binder: SQLParameterBinder<T>) throws {
+	///
+	/// - returns: `self`.
+	@discardableResult public func bind<T>(_ value: T, toParameter index: Int, _ binder: SQLParameterBinder<T>) throws -> Statement {
 		try binder.bind(self, value, index)
+		return self
 	}
 
 	/// Binds `value` to the SQL parameter named `name`.
@@ -53,7 +56,9 @@ extension Statement {
 	/// - parameter binder: The `SQLParameterBinder` to use for binding `value`.
 	///
 	/// - throws: An error if `value` couldn't be bound.
-	public func bind<T>(_ value: T, toParameter name: String, _ binder: SQLParameterBinder<T>) throws {
+	///
+	/// - returns: `self`.
+	@discardableResult public func bind<T>(_ value: T, toParameter name: String, _ binder: SQLParameterBinder<T>) throws -> Statement {
 		try bind(value, toParameter: indexOfParameter(name), binder)
 	}
 }
@@ -68,13 +73,16 @@ extension Statement {
 	/// - parameter binder: The `SQLParameterBinder` to use for binding `value`.
 	///
 	/// - throws: An error if `value` couldn't be bound.
-	public func bind<T>(_ value: Optional<T>, toParameter index: Int, _ binder: SQLParameterBinder<T>) throws {
+	///
+	/// - returns: `self`.
+	@discardableResult public func bind<T>(_ value: Optional<T>, toParameter index: Int, _ binder: SQLParameterBinder<T>) throws -> Statement {
 		switch value {
 		case .none:
 			try bindNull(toParameter: index)
 		case .some(let obj):
 			try bind(obj, toParameter: index, binder)
 		}
+		return self
 	}
 
 	/// Binds `value` or SQL NULL to the SQL parameter named `name`.
@@ -86,7 +94,9 @@ extension Statement {
 	/// - parameter binder: The `SQLParameterBinder` to use for binding `value`.
 	///
 	/// - throws: An error if `value` couldn't be bound.
-	public func bind<T>(_ value: Optional<T>, toParameter name: String, _ binder: SQLParameterBinder<T>) throws {
+	///
+	/// - returns: `self`.
+	@discardableResult public func bind<T>(_ value: Optional<T>, toParameter name: String, _ binder: SQLParameterBinder<T>) throws -> Statement {
 		try bind(value, toParameter: indexOfParameter(name), binder)
 	}
 }
@@ -148,21 +158,21 @@ extension Statement {
 extension SQLParameterBinder where T == String {
 	/// Binds a `String` object to an SQL parameter as a text value.
 	public static let string = SQLParameterBinder {
-		try $0.bind(text: $1, toParameter: $2)
+		try $0.bind($1, toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == Data {
 	/// Binds a `Data` object to an SQL parameter as a BLOB value.
 	public static let data = SQLParameterBinder {
-		try $0.bind(blob: $1, toParameter: $2)
+		try $0.bind($1, toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == Int {
 	/// Binds an `Int` object to an SQL parameter as a signed integer value.
 	public static let int = SQLParameterBinder {
-		try $0.bind(integer: Int64($1), toParameter: $2)
+		try $0.bind(Int64($1), toParameter: $2)
 	}
 }
 
@@ -170,56 +180,56 @@ extension SQLParameterBinder where T == UInt {
 	/// Binds a `UInt` object to an SQL parameter as a signed integer value.
 	/// - note: The unsigned integer value is bound as a bit pattern.
 	public static let uint = SQLParameterBinder {
-		try $0.bind(integer: Int64(Int(bitPattern: $1)), toParameter: $2)
+		try $0.bind(Int64(Int(bitPattern: $1)), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == Int8 {
 	/// Binds an `Int8` object to an SQL parameter as a signed integer value.
 	public static let int8 = SQLParameterBinder {
-		try $0.bind(integer: Int64($1), toParameter: $2)
+		try $0.bind(Int64($1), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == UInt8 {
 	/// Binds a `UInt8` object to an SQL parameter as a signed integer value.
 	public static let uint8 = SQLParameterBinder {
-		try $0.bind(integer: Int64($1), toParameter: $2)
+		try $0.bind(Int64($1), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == Int16 {
 	/// Binds an `Int16` object to an SQL parameter as a signed integer value.
 	public static let int16 = SQLParameterBinder {
-		try $0.bind(integer: Int64($1), toParameter: $2)
+		try $0.bind(Int64($1), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == UInt16 {
 	/// Binds a `UInt16` object to an SQL parameter as a signed integer value.
 	public static let uint16 = SQLParameterBinder {
-		try $0.bind(integer: Int64($1), toParameter: $2)
+		try $0.bind(Int64($1), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == Int32 {
 	/// Binds an `Int32` object to an SQL parameter as a signed integer value.
 	public static let int32 = SQLParameterBinder {
-		try $0.bind(integer: Int64($1), toParameter: $2)
+		try $0.bind(Int64($1), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == UInt32 {
 	/// Binds a `UInt32` object to an SQL parameter as a signed integer value.
 	public static let uint32 = SQLParameterBinder {
-		try $0.bind(integer: Int64($1), toParameter: $2)
+		try $0.bind(Int64($1), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == Int64 {
 	/// Binds an `Int64` object to an SQL parameter as a signed integer value.
 	public static let int64 = SQLParameterBinder {
-		try $0.bind(integer: $1, toParameter: $2)
+		try $0.bind($1, toParameter: $2)
 	}
 }
 
@@ -227,21 +237,21 @@ extension SQLParameterBinder where T == UInt64 {
 	/// Binds a `UInt64` object to an SQL parameter as a signed integer value.
 	/// - note: The unsigned integer value is interpreted as a bit pattern.
 	public static let uint64 = SQLParameterBinder {
-		try $0.bind(integer: Int64(bitPattern: $1), toParameter: $2)
+		try $0.bind(Int64(bitPattern: $1), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == Float {
 	/// Binds a `Float` object to an SQL parameter as a floating-point value.
 	public static let float = SQLParameterBinder {
-		try $0.bind(real: Double($1), toParameter: $2)
+		try $0.bind(Double($1), toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == Double {
 	/// Binds a `Double` object to an SQL parameter as a floating-point value.
 	public static let double = SQLParameterBinder {
-		try $0.bind(real: $1, toParameter: $2)
+		try $0.bind($1, toParameter: $2)
 	}
 }
 
@@ -249,7 +259,7 @@ extension SQLParameterBinder where T == Bool {
 	/// Binds a `Bool` object to an SQL parameter as a signed integer value.
 	/// - note: True is bound as 1 while false is bound as 0.
 	public static let bool = SQLParameterBinder {
-		try $0.bind(integer: $1 ? 1 : 0, toParameter: $2)
+		try $0.bind($1 ? Int64(1) : Int64(0), toParameter: $2)
 	}
 }
 
@@ -257,7 +267,7 @@ extension SQLParameterBinder where T == UUID {
 	/// Binds a `UUID` object as a text value.
 	/// - note: The value is bound as a lower case UUID string.
 	public static let uuidString = SQLParameterBinder {
-		try $0.bind(text: $1.uuidString.lowercased(), toParameter: $2)
+		try $0.bind($1.uuidString.lowercased(), toParameter: $2)
 	}
 
 	/// Binds a `UUID` object as a text value.
@@ -266,14 +276,14 @@ extension SQLParameterBinder where T == UUID {
 		let b = withUnsafeBytes(of: $1.uuid) {
 			Data($0)
 		}
-		try $0.bind(blob: b, toParameter: $2)
+		try $0.bind(b, toParameter: $2)
 	}
 }
 
 extension SQLParameterBinder where T == URL {
 	/// Binds a `URL` object as a text value.
 	public static let urlString = SQLParameterBinder {
-		try $0.bind(text: $1.absoluteString, toParameter: $2)
+		try $0.bind($1.absoluteString, toParameter: $2)
 	}
 }
 
@@ -281,20 +291,20 @@ extension SQLParameterBinder where T == Date {
 	/// Binds a `Date` object as a floating-point value.
 	/// - note: The value is bound as the number of seconds relative to 00:00:00 UTC on 1 January 1970.
 	public static let timeIntervalSince1970 = SQLParameterBinder {
-		try $0.bind(real: $1.timeIntervalSince1970, toParameter: $2)
+		try $0.bind($1.timeIntervalSince1970, toParameter: $2)
 	}
 
 	/// Binds a `Date` object as a floating-point value.
 	/// - note: The value is bound as the number of seconds relative to 00:00:00 UTC on 1 January 2001.
 	public static let timeIntervalSinceReferenceDate = SQLParameterBinder {
-		try $0.bind(real: $1.timeIntervalSinceReferenceDate, toParameter: $2)
+		try $0.bind($1.timeIntervalSinceReferenceDate, toParameter: $2)
 	}
 
 	/// Binds a `Date` object as a text value.
 	/// - parameter formatter: The formatter to use to generate the ISO 8601 date representation.
 	public static func iso8601DateString(_ formatter: ISO8601DateFormatter = ISO8601DateFormatter()) -> SQLParameterBinder {
 		SQLParameterBinder {
-			try $0.bind(text: formatter.string(from: $1), toParameter: $2)
+			try $0.bind(formatter.string(from: $1), toParameter: $2)
 		}
 	}
 }
@@ -304,7 +314,7 @@ extension SQLParameterBinder where T: Encodable {
 	/// - parameter encoder: The encoder to use to generate the encoded JSON data.
 	public static func json(_ encoder: JSONEncoder = JSONEncoder()) -> SQLParameterBinder {
 		SQLParameterBinder {
-			try $0.bind(blob: encoder.encode($1), toParameter: $2)
+			try $0.bind(encoder.encode($1), toParameter: $2)
 		}
 	}
 }
@@ -315,9 +325,9 @@ extension SQLParameterBinder where T == NSNumber {
 		switch CFNumberGetType(value as CFNumber) {
 		case .sInt8Type, .sInt16Type, .sInt32Type, .charType, .shortType, .intType,
 				.sInt64Type, .longType, .longLongType, .cfIndexType, .nsIntegerType:
-			try statement.bind(integer: value.int64Value, toParameter: index)
+			try statement.bind(value.int64Value, toParameter: index)
 		case .float32Type, .float64Type, .floatType, .doubleType, .cgFloatType:
-			try statement.bind(real: value.doubleValue, toParameter: index)
+			try statement.bind(value.doubleValue, toParameter: index)
 		@unknown default:
 			fatalError("Unexpected CFNumber type")
 		}
@@ -329,7 +339,7 @@ extension SQLParameterBinder where T: NSObject, T: NSCoding {
 	public static func nsKeyedArchive() -> SQLParameterBinder {
 		SQLParameterBinder { statement, value, index in
 			let b = try NSKeyedArchiver.archivedData(withRootObject: value, requiringSecureCoding: true)
-			try statement.bind(blob: b, toParameter: index)
+			try statement.bind(b, toParameter: index)
 		}
 	}
 }
